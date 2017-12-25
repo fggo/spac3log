83ccbbbec36fa5df1290c6cc64ff5e429c70cbf0
fggo <jnuho@outlook.com>
2017-12-25 20:15:47 +0900

    allow users to add or edit models

--- spac3logs/urls.py
+++ spac3logs/urls.py
@@ -12,11 +12,11 @@ urlpatterns = [
     # topic
     url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
     # new topic
-    # url(r'^new_topic/$', views.new_topic, name='new_topic'),
+    url(r'^new_topic/$', views.new_topic, name='new_topic'),
     # new entry
-    # url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry,
-    #     name='new_entry'),
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry,
+        name='new_entry'),
     # edit entry
-    # url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry,
-    #     name='edit_entry'),
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry,
+        name='edit_entry'),
 ]
--- spac3logs/views.py
+++ spac3logs/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Topic
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 def index(request):
     return render(request, 'spac3logs/index.html')
@@ -15,3 +18,47 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'spac3logs/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('spac3logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'spac3logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('spac3logs:topic',
+                                                args=[topic_id]))
+
+    context = {'form': form, 'topic': topic}
+    return render(request, 'spac3logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('spac3logs:topic',
+                                                args=[topic.id]))
+
+    context = {'topic': topic, 'entry': entry, 'form': form}
+    return render(request, 'spac3logs/edit_entry.html', context)
